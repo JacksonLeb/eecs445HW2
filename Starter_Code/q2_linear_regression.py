780b45ef0bfe53b5c3985a68387a938a5534d762
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2023-02-13 16:53:13 -0500

    working on getting GD working

---
+++ Starter_Code/q2_linear_regression.py
@@ -0,0 +1,202 @@
+"""
+EECS 445 - Introduction to Maching Learning
+HW2 Q2 Linear Regression Optimization Methods)
+~~~~~~
+Follow the instructions in the homework to complete the assignment.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from helper import load_data
+
+def calculate_empirical_risk(X, y, theta):
+    loss = 0
+    n = X.shape[0]
+
+    for i in range(n):
+        loss += (((y[i] - np.dot(theta,  X[i]))**2) / 2)
+    return loss / n
+
+def calculate_RMS_Error(X, y, theta):
+    """
+    Args:
+        X: np.array, shape (n, d) 
+        y: np.array, shape (n,)
+        theta: np.array, shape (d,). Specifies an (d-1)^th degree polynomial
+
+    Returns:
+        E_rms: float. The root mean square error as defined in the assignment.
+    """
+    # TODO: Implement this function
+    E_rms = 0
+    return E_rms
+
+
+def generate_polynomial_features(X, M):
+    """
+    Create a polynomial feature mapping from input examples. Each element x
+    in X is mapped to an (M+1)-dimensional polynomial feature vector 
+    i.e. [1, x, x^2, ...,x^M].
+
+    Args:
+        X: np.array, shape (n, 1). Each row is one instance.
+        M: a non-negative integer
+    
+    Returns:
+        Phi: np.array, shape (n, M+1)
+    """
+    # TODO: Implement this function
+    n = X.shape[0]
+    Phi = np.zeros((n, M+1))
+    for i, x in enumerate(X):
+        Phi[i][0] = 1
+        Phi[i][1] = x
+        for j in range(2, M + 1):
+            Phi[i][j] = pow(x, j)
+
+    return Phi
+
+
+def ls_gradient_descent(X, y, learning_rate=0):
+    """
+    Implements the Gradient Descent (GD) algorithm for least squares regression.
+    Note:
+        - Please use the stopping criteria: number of iterations >= 1e6 or |new_loss - prev_loss| <= 1e-10
+
+    Args:
+        X: np.array, shape (n, d) 
+        y: np.array, shape (n,)
+    
+    Returns:
+        theta: np.array, shape (d,)
+    """
+    n = X.shape[0]
+    d = X.shape[1]
+    theta = np.zeros((d,))
+    k = 0
+    new_loss = 0
+    prev_loss = 0
+    #convergence criteria
+    while(k < pow(10, 6)):
+        prev_loss = new_loss
+
+        print(f"new_loss: {new_loss}")
+        y_pred = X @ theta
+        grad = (2 * X.T @ (y_pred - y)) / n
+
+        theta = theta - (learning_rate * grad)
+        k += 1
+
+        new_loss = calculate_empirical_risk(X, y, theta)
+        if np.abs(new_loss - prev_loss) < pow(10, -10): 
+            print(f"ITERATIONS: {k}")
+            return theta
+    print(f"ITERATIONS: {k}")
+    return theta
+
+
+def ls_stochastic_gradient_descent(X, y, learning_rate=0):
+    """
+    Implements the Stochastic Gradient Descent (SGD) algorithm for least squares regression.
+    Note:
+        - Please do not shuffle your data points.
+        - Please use the stopping criteria: number of iterations >= 1e6 or |new_loss - prev_loss| <= 1e-10
+    
+    Args:
+        X: np.array, shape (n, d) 
+        y: np.array, shape (n,)
+    
+    Returns:
+        theta: np.array, shape (d,)
+    """
+    # TODO: Implement this function
+    theta = np.zeros((d,))
+    return theta
+
+
+def closed_form_optimization(X, y, reg_param=0):
+    """
+    Implements the closed form solution for least squares regression.
+
+    Args:
+        X: np.array, shape (n, d) 
+        y: np.array, shape (n,)
+        `reg_param`: float, an optional regularization parameter
+
+    Returns:
+        theta: np.array, shape (d,)
+    """
+    # TODO: Implement this function
+    theta = np.zeros((d,))
+    return theta
+
+
+def part_2_1(fname_train):
+    # TODO: This function should contain all the code you implement to complete 2.1. Please feel free to add more plot commands.
+    print("========== Part 2.1 ==========")
+
+    X_train, y_train = load_data(fname_train)
+    theta = ls_gradient_descent(X=generate_polynomial_features(X_train, 1), y=y_train, learning_rate=pow(10, -4))
+    poly = np.poly1d(theta)
+    print(theta)
+    y_theta = poly(X_train)
+    print("Done!")
+    plt.plot(X_train, y_train, 'ro')
+    plt.plot(X_train, y_theta, 'b')
+    plt.legend()
+    plt.savefig('q2_1.png', dpi=200)
+    plt.close()
+
+
+def part_2_2(fname_train, fname_validation):
+    # TODO: This function should contain all the code you implement to complete 2.2
+    print("=========== Part 2.2 ==========")
+
+    X_train, y_train = load_data(fname_train)
+    X_validation, y_validation = load_data(fname_validation)
+
+    # (a) OVERFITTING
+
+    errors_train = np.zeros((11,))
+    errors_validation = np.zeros((11,))
+    # Add your code here
+
+    plt.plot(errors_train,'-or',label='Train')
+    plt.plot(errors_validation,'-ob', label='Validation')
+    plt.xlabel('M')
+    plt.ylabel('$E_{RMS}$')
+    plt.title('Part 2.2.a')
+    plt.legend(loc=1)
+    plt.xticks(np.arange(0, 11, 1))
+    plt.savefig('q2_2_a.png', dpi=200)
+    plt.close()
+
+
+    # (b) REGULARIZATION
+
+    errors_train = np.zeros((10,))
+    errors_validation = np.zeros((10,))
+    L = np.append([0], 10.0 ** np.arange(-8, 1))
+    # Add your code here
+
+    plt.figure()
+    plt.plot(L, errors_train, '-or', label='Train')
+    plt.plot(L, errors_validation, '-ob', label='Validation')
+    plt.xscale('symlog', linthresh=1e-8)
+    plt.xlabel('$\lambda$')
+    plt.ylabel('$E_{RMS}$')
+    plt.title('Part 2.2.b')
+    plt.legend(loc=2)
+    plt.savefig('q2_2_b.png', dpi=200)
+    plt.close()
+
+    print("Done!")
+
+
+def main(fname_train, fname_validation):
+    X, y = load_data(fname_train)
+    part_2_1(fname_train)
+    part_2_2(fname_train, fname_validation)
+
+if __name__ == '__main__':
+    main("dataset/q2_train.csv", "dataset/q2_validation.csv")
