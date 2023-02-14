1aaa3fece99ccc92caf74bcaf935ff81f0da7650
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2023-02-13 21:01:57 -0500

    fnished up until 3

--- Starter_Code/q2_linear_regression.py
+++ Starter_Code/q2_linear_regression.py
@@ -8,11 +8,11 @@ Follow the instructions in the homework to complete the assignment.
 import numpy as np
 import matplotlib.pyplot as plt
 from helper import load_data
+from time import monotonic
 
 def calculate_empirical_risk(X, y, theta):
     loss = 0
     n = X.shape[0]
-
     for i in range(n):
         loss += (((y[i] - np.dot(theta,  X[i]))**2) / 2)
     return loss / n
@@ -29,6 +29,11 @@ def calculate_RMS_Error(X, y, theta):
     """
     # TODO: Implement this function
     E_rms = 0
+    E_err = 0
+    n = X.shape[0]
+    for i in range(n):
+        E_err += (np.dot(theta, X[i]) - y[i])**2
+    E_rms = np.sqrt(E_err/n)
     return E_rms
 
 
@@ -48,6 +53,9 @@ def generate_polynomial_features(X, M):
     # TODO: Implement this function
     n = X.shape[0]
     Phi = np.zeros((n, M+1))
+    if(M == 0):
+        return X
+
     for i, x in enumerate(X):
         Phi[i][0] = 1
         Phi[i][1] = x
@@ -77,20 +85,25 @@ def ls_gradient_descent(X, y, learning_rate=0):
     new_loss = 0
     prev_loss = 0
     #convergence criteria
+    start_time = monotonic()
     while(k < pow(10, 6)):
         prev_loss = new_loss
+        
+        y_pred = np.dot(X, theta)
+        diff = y_pred - y
+        grad = np.dot(X.T, diff) / n
 
-        print(f"new_loss: {new_loss}")
-        y_pred = X @ theta
-        grad = (2 * X.T @ (y_pred - y)) / n
 
         theta = theta - (learning_rate * grad)
         k += 1
 
         new_loss = calculate_empirical_risk(X, y, theta)
+        
         if np.abs(new_loss - prev_loss) < pow(10, -10): 
+            print(f"FINAL TIME: {monotonic() - start_time}")
             print(f"ITERATIONS: {k}")
             return theta
+    print(f"FINAL TIME: {monotonic() - start_time}")
     print(f"ITERATIONS: {k}")
     return theta
 
@@ -110,7 +123,29 @@ def ls_stochastic_gradient_descent(X, y, learning_rate=0):
         theta: np.array, shape (d,)
     """
     # TODO: Implement this function
+    n = X.shape[0]
+    d = X.shape[1]
     theta = np.zeros((d,))
+    k = 0
+    new_loss = 0
+    prev_loss = 0
+    #convergence criteria
+    start_time = monotonic()
+    while(k < pow(10, 6)):
+        prev_loss = new_loss
+        #np.random.shuffle(X)
+        #np.random.shuffle(y)
+        for i in range(n):
+            theta = theta + ((learning_rate * (y[i] - np.dot(theta, X[i])))*X[i])
+            k += 1
+
+        new_loss = calculate_empirical_risk(X, y, theta)
+        if np.abs(new_loss - prev_loss) < pow(10, -10): 
+            print(f"FINAL TIME: {monotonic() - start_time}")
+            print(f"ITERATIONS: {k}")
+            return theta
+    print(f"FINAL TIME: {monotonic() - start_time}")
+    print(f"ITERATIONS: {k}")
     return theta
 
 
@@ -127,7 +162,11 @@ def closed_form_optimization(X, y, reg_param=0):
         theta: np.array, shape (d,)
     """
     # TODO: Implement this function
+    n = X.shape[0]
+    d = X.shape[1]
     theta = np.zeros((d,))
+
+    theta = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X) + np.identity(d) * reg_param), X.T), y) + (reg_param / 2)
     return theta
 
 
@@ -136,9 +175,10 @@ def part_2_1(fname_train):
     print("========== Part 2.1 ==========")
 
     X_train, y_train = load_data(fname_train)
-    theta = ls_gradient_descent(X=generate_polynomial_features(X_train, 1), y=y_train, learning_rate=pow(10, -4))
-    poly = np.poly1d(theta)
+    theta = closed_form_optimization(X=generate_polynomial_features(X_train, 1), y=y_train)
+    print("RESULTS: ")
     print(theta)
+    poly = np.poly1d(theta)
     y_theta = poly(X_train)
     print("Done!")
     plt.plot(X_train, y_train, 'ro')
@@ -160,6 +200,12 @@ def part_2_2(fname_train, fname_validation):
     errors_train = np.zeros((11,))
     errors_validation = np.zeros((11,))
     # Add your code here
+    '''
+    for i in range(11):
+        theta = closed_form_optimization(generate_polynomial_features(X_train, i), y_train)
+        errors_train[i] = calculate_RMS_Error(generate_polynomial_features(X_train, i), y_train, theta)
+        errors_validation[i] = calculate_RMS_Error(generate_polynomial_features(X_validation, i), y_validation, theta)
+
 
     plt.plot(errors_train,'-or',label='Train')
     plt.plot(errors_validation,'-ob', label='Validation')
@@ -171,13 +217,27 @@ def part_2_2(fname_train, fname_validation):
     plt.savefig('q2_2_a.png', dpi=200)
     plt.close()
 
-
+    '''
     # (b) REGULARIZATION
 
     errors_train = np.zeros((10,))
     errors_validation = np.zeros((10,))
     L = np.append([0], 10.0 ** np.arange(-8, 1))
     # Add your code here
+    best_score = 10000000000000
+    best_theta = 0
+    for lam in L:
+        temp_theta = closed_form_optimization(generate_polynomial_features(X_train, 10), y_train, lam)
+        temp_score = calculate_empirical_risk(generate_polynomial_features(X_train, 10), y_train, temp_theta)
+
+        if(temp_score < best_score):
+            best_score = temp_score
+            best_theta = temp_theta
+
+    for i in range(10):
+        theta = closed_form_optimization(generate_polynomial_features(X_train, 10), y_train, L[i])
+        errors_train[i] = calculate_RMS_Error(generate_polynomial_features(X_train, 10), y_train, theta)
+        errors_validation[i] = calculate_RMS_Error(generate_polynomial_features(X_validation, 10), y_validation, theta)
 
     plt.figure()
     plt.plot(L, errors_train, '-or', label='Train')
